9931951f21dacb16511e00cb8105d2fda3775022
marcoacevedogarrido <marcoacevedogarrido@gmail.com>
2024-08-31 21:28:03 -0400

    login-register

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'server',
+    'dj_rest_auth',
 ]
 
 
