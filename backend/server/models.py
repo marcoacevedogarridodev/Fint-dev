445264157ade6cdadce1dc13aee0cba7b4b765b3
marcoacevedogarrido <marcoacevedogarrido@gmail.com>
2024-09-02 02:48:15 -0400

    task_diario

--- backend/server/models.py
+++ backend/server/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Indicador(models.Model):
     uf = models.DecimalField(max_digits=10, decimal_places=2)
-    uvp = models.DecimalField(max_digits=10, decimal_places=2)
-    fecha = models.DateField()
+
  
 
--- backend/server/tasks.py
+++ backend/server/tasks.py
@@ -2,15 +2,41 @@ import requests
 from datetime import datetime
 from .models import Indicador  
 from celery import shared_task
+import sqlite3
 
 @shared_task
 def fetch_mindicador_data():
     try:
-        response = requests.get('https://mindicador.cl/api/uf')
+        response = requests.get('https://mindicador.cl/api/uf/')
         response.raise_for_status() 
         data = response.json()
-        print(data) 
-        return data
+        
+        if data['serie']:
+            primer_valor = data['serie'][0]['valor']
+            save_to_database(primer_valor)
+            return primer_valor
+        else:
+            print("No data available.")
+            return None
+     
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
         return None
+
+def save_to_database(valor):
+    conn = sqlite3.connect('db.sqlite3')
+    cursor = conn.cursor()    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS server_indicador (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            uf INTEGER,
+            fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    ''')
+    
+    cursor.execute('INSERT INTO server_indicador (uf) VALUES (?)', (valor,))
+    
+    conn.commit()
+    conn.close()
+
+fetch_mindicador_data()
